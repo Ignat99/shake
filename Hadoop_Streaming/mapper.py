a2dfdafc45a4e4ed407a7450a7bfa3d7cd2becd3
ignat99 <ignat99@gmail.com>
2018-11-07 00:00:25 +0300

    tested 1

--- Hadoop_Streaming/mapper.py
+++ Hadoop_Streaming/mapper.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 #mapper.py
 import sys
 
 def do_map(doc):
-for word in doc.split():
-    yield word.lower(), 1
+    for word in doc.split():
+        yield word.lower(), 1
 
 for line in sys.stdin:
     for key, value in do_map(line):
--- Hadoop_Streaming/run.sh
+++ Hadoop_Streaming/run.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 
-rm -r masterpieces
-rm -r masterpieces_wordcount
-mkdir masterpieces
-mkdir masterpieces_wordcount
+#rm -r masterpieces
+#rm -r masterpieces_wordcount
+#mkdir masterpieces
+#mkdir masterpieces_wordcount
 
-wget https://storage.cloud.google.com/apache-beam-samples/shakespeare/kinglear.txt
-mv kinglear.txt masterpieces
+#wget https://storage.cloud.google.com/apache-beam-samples/shakespeare/kinglear.txt
+#mv kinglear.txt masterpieces
 
-wget https://storage.cloud.google.com/apache-beam-samples/shakespeare/othello.txt
-mv othello.txt masterpieces
+#wget https://storage.cloud.google.com/apache-beam-samples/shakespeare/othello.txt
+#mv othello.txt masterpieces
 
-wget https://storage.cloud.google.com/apache-beam-samples/shakespeare/romeoandjuliet.txt
-mv romeoandjuliet.txt masterpieces
+#wget https://storage.cloud.google.com/apache-beam-samples/shakespeare/romeoandjuliet.txt
+#mv romeoandjuliet.txt masterpieces
 
-hadoop fs -put masterpieces
+#hadoop fs -put masterpieces
 
-yarn jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar\
+#yarn jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar\
+#mapred streaming\
+yarn jar /opt/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar\
  -input masterpieces\
  -output masterpieces_wordcount\
+ -mapper mapper.py\
+ -reducer reducer.py\
  -file mapper.py\
- -file reducer.py\
- -mapper "python mapper.py"\
- -reducer "python reducer.py"\
- -combiner "python reducer.py"
+ -file reducer.py
+
+# -mapper /bin/cat \
+# -reducer /usr/bin/wc\
+# -D mapreduce.job.reduces=0
+
+# -combiner "python reducer.py"
 
 
 hadoop fs -text masterpieces_wordcount/* | sort -n -k2,2 | tail -n5
