9eaeb314ec7827da4fba998c39d1860da25be0c6
ignat99 <ignat99@gmail.com>
2018-11-10 20:46:44 +0300

    Spark Extra

--- shake.py
+++ shake.py
@@ -20,3 +20,27 @@ def stem(doc):
 RDDread = sc.textFile ("file:///home/ubuntu/shake/mas/shakespeare_*.txt")
 #stemmed_docs = clean_text_rdd.map(tokenize).map(stem).cache()
 stemmed_docs = RDDread.map(tokenize).map(stem).cache()
+
+# Calculate Word counts
+
+def get_words(doc):
+    return [(word, 1) for word in set(doc['stemmed'])]
+
+word_counts = stemmed_docs.flatMap(get_words)\
+            .reduceByKey(lambda x, y: x+y).collect()
+
+# Calculate Inverse Document Frequency (IDF)
+
+doc_count = stemmed_docs.count()
+
+def get_idf(doc_count, doc_with_word_count):
+    return math.log(doc_count/doc_with_word_count)
+
+idf = word_counts.mapValues(lambda word_count: get_idf(doc_count, word_count))
+
+# Get list of stop words
+
+idf_border = 1.12
+stop_words_list =idf.filter(lambda x: x[1] < idf_border).keys().collect()
+stop_words = set(sop_words)
+
